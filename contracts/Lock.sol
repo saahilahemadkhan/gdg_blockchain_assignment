5ac0d684bb01c6ed6a8f853b1c623c05a6b2f0d1
saahilahemadkhan <saahilahemadkhan@gmail.com>
2025-09-03 12:19:18 +0530

    Update Lock.sol

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,34 +1,16 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.28;
+// SPDX-Lisence-Identifier: MIT
+pragma solidity ^0.8.18; //Solidity Version
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
-
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
-
-    event Withdrawal(uint amount, uint when);
-
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+contract PersonalLocker {
+    string public message
+    struct person{
+        string owner;
+        string message;
+        uint256 password;
     }
+person[] public listofpeople;
+function addperson(string memory owner , string memory message, uint256 password) public{
+        listofpeople.push(person(owner,message,password));
+}
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
-    }
 }
